51979875dcc40cdd5b8ac16f86525823ad0757fe
Borroot <borroot@live.nl>
2020-12-06 00:07:45 +0100

    Add wall texture rendering.

--- src/render.c
+++ src/render.c
@@ -4,6 +4,7 @@
 #include "color.h"
 #include "point.h"
 #include "state.h"
+#include "texture.h"
 
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
@@ -18,22 +19,24 @@ static void render_line(SDL_Renderer *renderer, State *state, size_t x)
 	int side;  /* what side was hit of the wall x = 0 or y = 1 */
 	float wall_dst = collide_ray(state, &ray, &cell, &side);  /* perp dist */
 
+	size_t index = (state->level->map[state->level->w * cell.y + cell.x]-1)*2;
+	if (!side) index++;  /* choose dark texture variant */
+	Texture texture = texture_walls[index];
+
+	float wall_hit;  /* coordinate at which the wall is hit */
+	if (side) wall_hit = state->pos.x + wall_dst * ray.x;
+	else      wall_hit = state->pos.y + wall_dst * ray.y;
+
+	int texture_x = (int)((wall_hit - (int)wall_hit) * (float)texture.w);
+	if(side == 0 && ray.x > 0) texture_x = texture.w - texture_x - 1;
+	if(side == 1 && ray.y < 0) texture_x = texture.w - texture_x - 1;
+
 	int line_height = (int)(SCREEN_HEIGHT / wall_dst);
-	int line_top = MAX(0, -line_height / 2 + SCREEN_HEIGHT / 2);
-	int line_bot = MIN(SCREEN_HEIGHT - 1, line_height / 2 + SCREEN_HEIGHT / 2);
+	int line_top = SCREEN_HEIGHT / 2 - line_height / 2;
 
-	SDL_Color color;
-	switch (state->level->map[state->level->w * cell.y + cell.x]) {
-		case 1:  color = COLOR_RED;    break;
-		case 2:  color = COLOR_GREEN;  break;
-		case 3:  color = COLOR_WHITE;  break;
-		case 4:  color = COLOR_BLUE;   break;
-		default: color = COLOR_YELLOW; break;
-	}
-	if (side) { color.r /= 2; color.g /= 2; color.b /= 2; }
-
-	SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
-	SDL_RenderDrawLine(renderer, x, line_top, x, line_bot);
+	SDL_Rect srcrect = {texture_x, 0, 1, texture.h};
+	SDL_Rect dstrect = {x, line_top, 1, line_height};
+	SDL_RenderCopy(renderer, texture.img, &srcrect, &dstrect);
 }
 
 static void render_floor(SDL_Renderer *renderer, State *state)
